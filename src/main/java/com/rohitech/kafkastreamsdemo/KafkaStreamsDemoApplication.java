62979ee25d0f05ab4fbfdf26a38e53d41557b322
rohitsingh341 <rohitsingh341@gmail.com>
2024-04-12 16:28:50 +0100

    Kafka Streams Demo - Word Count

---
+++ src/main/java/com/rohitech/kafkastreamsdemo/KafkaStreamsDemoApplication.java
@@ -0,0 +1,48 @@
+package com.rohitech.kafkastreamsdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Named;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+@Slf4j
+@SpringBootApplication
+public class KafkaStreamsDemoApplication {
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-app");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, String> kStream = streamsBuilder.stream("word-count-input");
+
+        KTable<String, Long> countKTable = kStream.mapValues(value -> value.toLowerCase())
+                .flatMapValues(lowerCasedLine -> Arrays.asList(lowerCasedLine.split(" ")))
+                .selectKey((key, value) -> value)
+                .groupByKey()
+                .count(Named.as("Word-Count"));
+
+        countKTable.toStream().to("word-count-output");
+
+        KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+        kafkaStreams.start();
+
+        log.info(kafkaStreams.toString());
+    }
+
+}
